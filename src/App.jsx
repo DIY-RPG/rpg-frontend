2d4a3bece7763a993f5a192fbdbf0d83e9c4e193
Kevindroide <kevindroide@gmail.com>
2022-03-31 23:39:36 -0600

    Typescript integration

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Character from './Character';
+import './character.css'
+
+function App() {
+  return (
+    <div className="background-containter"> 
+      <div className="main-container">
+         <Character/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
