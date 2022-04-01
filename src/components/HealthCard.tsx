724bbd0b010b3c7d95454c096903158884eb5130
Kevindroide <kevindroide@gmail.com>
2022-03-31 23:49:57 -0600

    HealtCard TS

---
+++ src/components/HealthCard.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import { CharacterHP } from "../Structs";
+import './components.css'
+
+export const HPCard = (props:CharacterHP) => {
+
+    const [HP, setHP] = useState(props.current)
+
+    return (
+       <div className="props-card">
+           <text>HP</text>
+           <div className="props-value">
+               <div style={{display: 'flex', flexDirection: 'column', alignSelf:'auto'}}>
+               <button className="sec-button" onClick={() => setHP(HP+1)}>+</button>
+               <button className="sec-button" onClick={() => setHP(HP-1)}>-</button>
+               </div>
+               <text>{HP}</text>
+            </div>
+       </div>
+    );
+}
+
+export default HPCard;
