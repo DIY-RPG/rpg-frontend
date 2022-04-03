03408e58f8c3ebdfe2a822f682bcedd0f2bedb3f
Kevindroide <kevindroide@gmail.com>
2022-04-02 19:11:36 -0600

    Character spells implementation

--- src/Character.tsx
+++ src/Character.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import CharacterAction, { CharacterActionInterface } from "./characterComponents/ActionCard";
-import { CharacterSpell } from "./characterComponents/SpellCard";
+import { CharacterSpell, CharacterSpellInterface } from "./characterComponents/SpellCard";
 import CharacterSheet from "./CharacterSheet";
-import { Stat, DefaultColors, Ability, CharacterAbilities, CharacterSkills, Item} from "./Structs";
+import { Stat, DefaultColors, Ability, CharacterAbilities, CharacterSkills, Item, SpellGradeMap, SpellGrade} from "./Structs";
 
 export interface  CharacterProps {
 
@@ -22,7 +22,7 @@ export interface  CharacterState {
     initiative: number;
     speed: number;
     actions: Array<CharacterActionInterface>;
-    spells: Array<CharacterSpell>;
+    spells: Array<CharacterSpellInterface>;
     inventory: Array<Item>;
     equipment: Array<Item>;
     notes: Array<string>;
@@ -93,19 +93,65 @@ class Character extends Component< CharacterProps,  CharacterState> {
                 criticalDescription: "",
                 notes: "",
             },
+            {
+                name: "Poisoned Knife",
+                type: "Melee",
+                range: "5",
+                toHit: "1d20",
+                damage: "1d8",
+                damageType: "Piercing",
+                description: "You attack a creature you can see within 5 feet of you. You make a melee weapon attack against the target. If the attack hits, you add your Charisma modifier to the attack roll. You have advantage on the attack roll if the target is invisible, or the target is behind cover and you are able to see it.",
+                attack: "1d20 + Charisma modifier",
+                advantage: true,
+                disadvantage: false,
+                critical: false,
+                criticalMultiplier: 2,
+                criticalRange: 20,
+                criticalDamage: "1d8",
+                criticalDamageType: "Piercing",
+                criticalDescription: "",
+                notes: "",
+            },
+
         ],
-        spells: [],
+        spells: [
+            {
+                name: "Fireball",
+                school: "Evocation",
+                range: "60",
+                grade: "Cantrip",
+                castingTime: "1 action",
+                components: ["V", "S", "M"],
+                duration: "Instantaneous",
+                savingThrow: {
+                    stat: "INT",
+                    value: 12,
+                    modifier: "WIS",
+                    proficient: true,
+                    },
+                description: "A bright streak flashes from your pointing finger to a point you choose within range and then blossoms with a low roar into an explosion of flame. Each creature in a 20-foot radius must make a Dexterity saving throw. A target takes 8d6 fire damage on a failed save, or half as much damage on a successful one. The fire spreads around corners. It ignites flammable objects in the area that aren't being worn or carried.",
+                attackBonus : 5, 
+                damage: {
+                    type: "Fire",
+                    amount: "3d6",
+                },
+                notes: "",
+                
+
+            },
+            
+        ],
         inventory: [],
         equipment: [],
         notes: [],
 
         stats: {
-            STR : { label: "STR", name:"Strength", value: 0, modifier: 0, proficient: false},
-            DEX : { label: "DEX", name:"Dexterity", value: 0, modifier: 0, proficient: false},
-            CON : { label: "CON", name:"Constitution", value: 0, modifier: 0, proficient: false},
-            INT : { label: "INT", name:"Intelligence", value: 0, modifier: 0, proficient: false},
-            WIS : { label: "WIS", name:"Wisdom", value: 0, modifier: 0, proficient: false},
-            CHA : { label: "CHA", name:"Charisma", value: 0, modifier: 0, proficient: false}
+            STR : { label: "STR", name:"Strength", value: 12, modifier: 1, proficient: false},
+            DEX : { label: "DEX", name:"Dexterity", value: 13, modifier: 4, proficient: false},
+            CON : { label: "CON", name:"Constitution", value: 10, modifier: 4, proficient: false},
+            INT : { label: "INT", name:"Intelligence", value: 14, modifier: 2, proficient: false},
+            WIS : { label: "WIS", name:"Wisdom", value: 15, modifier: 5, proficient: false},
+            CHA : { label: "CHA", name:"Charisma", value: 0, modifier: 1, proficient: false}
         },
 
         skills: {
